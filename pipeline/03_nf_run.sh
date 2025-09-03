2510f4ab1945b32fbf80b4063318609d2ca5717c
Jason Stajich <jasonstajich.phd@gmail.com>
2025-09-02 17:58:16 -0700

    sync

--- pipeline/03_nf_run.sh
+++ pipeline/03_nf_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash -l
-#SBATCH -p epyc -n 1 -N 1 -c 24 --mem 32gb --out logs/nf.log --time 3-0:0:0
+#SBATCH -p epyc -n 1 -N 1 -c 24 --mem 192gb --out logs/nf.log --time 3-0:0:0
 
 module load singularity
 
